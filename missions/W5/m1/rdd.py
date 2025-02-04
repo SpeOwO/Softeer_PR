7dba20a4618cc1d096f35e2ab3ae5350c611e524
김태현 <speowo@gimtaehyeon-ui-MacBookPro.local>
2025-02-04 15:41:34 +0900

    feature: w5m1 transforming

--- missions/W5/m1/rdd.py
+++ missions/W5/m1/rdd.py
@@ -1,13 +1,43 @@
 from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext
 
-# SparkSession 생성
+year = 2022
+month = 1
+
+# SparkSession
 spark = SparkSession.builder \
-    .appName("PySpark Parquet Example") \
+    .appName("w5 m2") \
     .getOrCreate()
 
-# Parquet 파일 읽기
+# reading parquet
 df = spark.read.parquet("hdfs://namenode:9000/20221.parquet")
 
+# DataFrame to RDD
+rdd = df.rdd
 
-df.show()
+# filtering
+filtered_rdd = rdd.filter(lambda row: (row.fare_amount > 0) and (row.tpep_pickup_datetime.year == year) and (row.tpep_pickup_datetime.month == month))
+
+# Trips count
+total_trips = filtered_rdd.count()
+
+# total revenue, total distance
+total_revenue, total_distance = filtered_rdd.map(lambda row: (row.total_amount, row.trip_distance)) \
+    .reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+avg_distance = total_distance / total_trips
+
+# rdd by date
+date_rdd = filtered_rdd.map(lambda row: (row.tpep_pickup_datetime.date(), (1, row.total_amount))) \
+    .reduceByKey(lambda x, y : (x[0] + y[0], x[1] + y[1]))
+
+c1 = date_rdd.collect()
+
+# print
+print(f"Total trips: {total_trips}")
+print(f"Total revenue: {total_revenue}")
+print(f"Avg distance: {avg_distance}")
+
+for key, value in c1:
+    print(key, value[0], value[1])
+    
+# Spark stop
+spark.stop()
